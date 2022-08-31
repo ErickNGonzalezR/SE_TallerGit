fddc2a0ff9e990d4bba34514fddb6a13358ec464
Erick Gonzalez <egonzalez48@uan.edu.co>
2022-08-31 11:06:25 -0500

    ahora permite recibir su nombre como argumento

--- hola.py
+++ hola.py
@@ -1,3 +1,8 @@
-def say_hello(name="word"):
-    print("hello", name,"!")
-say_hello()    
+import argparse
+def say_hello(name="World"):
+    print("Hello ", name, "!")
+    parser = argparse.ArgumentParser(description='Say hello.')
+    parser.add_argument('--name', type=str, required=False,
+    metavar='name', help='your name', default='World')
+    args = parser.parse_args()
+say_hello(args.name)
